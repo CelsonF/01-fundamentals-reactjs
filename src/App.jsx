a095e9dc6358783e568f4a93936112758567862b
CelsonF <celsondev@outlook.com>
2023-09-27 00:36:14 -0300

    create css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Post } from "./components/Post"
+import { Header } from "./components/Header"
 
+import "./assets/css/global.css"
 
 export function App() {
   return (
-    <>
+    <div>
+      <Header/>
       <Post 
         author="Celson Fernando" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
       />
       
-    </>
+    </div>
   )
 }
