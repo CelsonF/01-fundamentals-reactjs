8f02522230bfabbe45d645c94e6ac1c6961ead13
CelsonF <celsondev@outlook.com>
2023-10-07 22:37:43 -0300

    Create component comment and your module css

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp , Trash } from '@phosphor-icons/react/dist/ssr';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://avatars.githubusercontent.com/u/29209254?v=4" alt="Celson Fernando" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <section className={styles.authorTime}>
+                            <strong> Celson Fernando </strong>
+                            <time title="04 de Outubro às 21h:29m" dateTime="2023-10-04 21:29:00"> Cerca de 1h atrás</time>
+                        </section>
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p> Ficou muito bom, parabéns! </p>
+                </div> 
+            
+            
+                <footer>
+                  <button>
+                    <ThumbsUp size={20} />
+                    Aplaudir <span> 20 </span>
+                  </button>
+                </footer>
+            
+            </div>
+        </div>
+    )
+} 
